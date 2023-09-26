849665b8299edae190661b06b16dcb2036972dd5
krish4uu <prajapatikashyap97@gmail.com>
2023-09-26 17:51:41 -0400

    Updated img path

--- app/images.ts
+++ app/images.ts
@@ -2,23 +2,23 @@ export const usaImg = {
   img: [
     {
       id: 1,
-      src: "./usa/mit-1.webp",
+      src: "../../usa/mit-1.webp",
     },
     {
       id: 2,
-      src: "./usa/mit-2.webp",
+      src: "../../usa/mit-2.webp",
     },
     {
       id: 3,
-      src: "./usa/mit-3.webp",
+      src: "../../usa/mit-3.webp",
     },
     {
       id: 4,
-      src: "./usa/mit-4.webp",
+      src: "../../usa/mit-4.webp",
     },
     {
       id: 5,
-      src: "./usa/mit-5.webp",
+      src: "../../usa/mit-5.webp",
     },
     {
       id: 6,
@@ -26,31 +26,31 @@ export const usaImg = {
     },
     {
       id: 7,
-      src: "./usa/mit-7.webp",
+      src: "../../usa/mit-7.webp",
     },
     {
       id: 8,
-      src: "./usa/mit-8.webp",
+      src: "../../usa/mit-8.webp",
     },
     {
       id: 9,
-      src: "./usa/mit-9.webp",
+      src: "../../usa/mit-9.webp",
     },
     {
       id: 10,
-      src: "./usa/mit-10.webp",
+      src: "../../usa/mit-10.webp",
     },
     {
       id: 11,
-      src: "./usa/mit-11.webp",
+      src: "../../usa/mit-11.webp",
     },
     {
       id: 12,
-      src: "./usa/mit-12.webp",
+      src: "../../usa/mit-12.webp",
     },
     {
       id: 15,
-      src: "./usa/mit-15.webp",
+      src: "../../usa/mit-15.webp",
     },
   ],
 };
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="fixed z-50 left-0 top-0 p-2 content-between flex justify-center from-zinc-200  backdrop-blur-xl dark:bg-zinc-800/30 dark:from-inherit static w-full lg:bg-gray-200 lg:dark:bg-zinc-800/30">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/ganesh.svg"
+          src="../../ganesh.svg"
           alt="Ganesh Logo"
           width={0}
           height={0}

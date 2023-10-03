acc7e3b4959a0127dea39b853030291887923fb6
krish4uu <prajapatikashyap97@gmail.com>
2023-10-03 15:49:10 -0400

    Updated wedding details

--- app/functionDetail.ts
+++ app/functionDetail.ts
@@ -5,43 +5,43 @@ export const functionDetail = [
     img: "./gold.jpeg",
     imgAlt: "Mameru image",
     date: "23th November 2023",
-    time: "7:00 pm",
+    time: "4:30 pm",
     address: "7, Suramya Bungalows",
     map: "https://maps.app.goo.gl/Xt1feBqLXjbR7LVg7",
-    countdownTime: "November 23, 2023 19:00:00"
+    countdownTime: "November 23, 2023 16:30:00"
   },
   {
     id: 2,
     name: "Graha Shanti",
     img: "./ganpati.webp",
     imgAlt: "Ganpati image",
-    date: "23th November 2023",
-    time: "11:00 am",
+    date: "24th November 2023",
+    time: "10:00 am",
     address: "7, Suramya Bungalows",
     map: "https://maps.app.goo.gl/Xt1feBqLXjbR7LVg7",
-    countdownTime: "November 23, 2023 11:00:00"
+    countdownTime: "November 24, 2023 10:00:00"
   },
   {
     id: 3,
     name: "Dandiya Raas",
     img: "./sangeet.jpeg",
     imgAlt: "sangeet image",
-    date: "23th November 2023",
-    time: "7:00 pm",
+    date: "24th November 2023",
+    time: "9:00 pm",
     address: "Vilaj Party plot",
     map: "https://maps.app.goo.gl/6re3CGMDx7VLMFbr9",
-    countdownTime: "November 23, 2023 19:00:00"
+    countdownTime: "November 24, 2023 21:00:00"
   },
   {
     id: 4,
     name: "Haldi",
     img: "./haldi-1.jpeg",
     imgAlt: "Haldi image",
-    date: "24th November 2023",
-    time: "12:00 pm",
+    date: "25th November 2023",
+    time: "3:00 pm",
     address: "7, Suramya Bungalows",
     map: "https://maps.app.goo.gl/Xt1feBqLXjbR7LVg7",
-    countdownTime: "November 24, 2023 12:00:00"
+    countdownTime: "November 25, 2023 15:00:00"
   },
   {
     id: 5,
@@ -49,20 +49,20 @@ export const functionDetail = [
     img: "./weeding.jpeg",
     imgAlt: "Wedding image",
     date: "25th November 2023",
-    time: "6:00 pm",
+    time: "8:00 pm",
     address: "7, Suramya Bungalows",
     map: "https://maps.app.goo.gl/Xt1feBqLXjbR7LVg7",
-    countdownTime: "November 25, 2023 18:00:00"
+    countdownTime: "November 25, 2023 20:00:00"
   },
   {
     id: 6,
     name: "Reception",
     img: "./reception.jpeg",
     imgAlt: "Reception image",
-    date: "26th November 2023",
-    time: "5:00 pm",
+    date: "27th November 2023",
+    time: "7:00 pm",
     address: "Aashirwad Party Plot",
     map: "https://maps.app.goo.gl/jiVzfzkBhEoVhEA2A",
-    countdownTime: "November 26, 2023 17:00:00"
+    countdownTime: "November 27, 2023 19:00:00"
   },
 ];
